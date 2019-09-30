05858a7254084822a02226fa5ceaef6db2f8322e
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-30 15:53:29 -0500

    added form data and connected to mongodb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -9,29 +11,55 @@ export class AppComponent {
   title = "intakeQR";
   qrcodedata: string;
   elementType: "url" | "canvas" | "img" = "url";
-  value: string;
+  value: any;
   display = false;
   href: string;
-  onFileChanged(event) {
-    const files = event.target.files;
-    if (files.length === 0) return;
-    var mimeType = files[0].type;
-    if (mimeType.match(/image\/*/) == null) {
-      alert("Only images are supported.");
-      return;
-    }
-    var reader = new FileReader();
-    reader.readAsDataURL(files[0]);
-    reader.onload = _event => {
-      this.qrcodedata = reader.result.toString();
-      console.log(reader.result);
-    };
+  SERVER_URL = "https://lacy-ringer.glitch.me/intake";
+  uploadForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.uploadForm = this.formBuilder.group({
+      name: [""],
+      age: [""],
+      location: [""],
+      type: [""],
+      img: [""]
+    });
+    console.log(this.uploadForm.value);
   }
-  generateQRCode() {
-    this.value = this.qrcodedata;
+
+  onHandleChange = event => {
+    this.uploadForm.get(event.target.name).setValue(event.target.value);
+    console.log(this.uploadForm.value);
+  };
+
+  onFileSelect(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.uploadForm.get("img").setValue(file);
+    }
+    console.log(this.uploadForm.value);
+  }
+
+  generateQRCode(res) {
+    console.log(res);
+    this.value = "https://www.google.com/";
     this.display = true;
   }
-  downloadImage() {
-    this.href = document.getElementsByTagName("img")[0].src;
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append("name", this.uploadForm.get("name").value);
+    formData.append("age", this.uploadForm.get("age").value);
+    formData.append("location", this.uploadForm.get("location").value);
+    formData.append("type", this.uploadForm.get("type").value);
+    // formData.append("file", this.uploadForm.get("img").value);
+    this.httpClient
+      .post<any>(this.SERVER_URL, formData)
+      .subscribe(res => this.generateQRCode(res), err => console.log(err));
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 import { NgxQRCodeModule } from 'ngx-qrcode2'; 
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HttpModule, NgxQRCodeModule],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule, HttpClientModule, NgxQRCodeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
