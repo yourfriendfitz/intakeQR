9c1ebb7788233c057f0552d8de05fecef53466b3
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-30 10:55:41 -0500

    added QR code reader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'intakeQR';
+  title = "intakeQR";
+  qrcodedata: string;
+  elementType: "url" | "canvas" | "img" = "url";
+  value: string;
+  display = false;
+  href: string;
+  onFileChanged(event) {
+    const files = event.target.files;
+    if (files.length === 0) return;
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      alert("Only images are supported.");
+      return;
+    }
+    var reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = _event => {
+      this.qrcodedata = reader.result.toString();
+      console.log(reader.result);
+    };
+  }
+  generateQRCode() {
+    this.value = this.qrcodedata;
+    this.display = true;
+  }
+  downloadImage() {
+    this.href = document.getElementsByTagName("img")[0].src;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { NgxQRCodeModule } from 'ngx-qrcode2'; 
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpModule, NgxQRCodeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
