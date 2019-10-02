3a166295d203c57bf427f9513d08f637515d7f57
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-10-02 15:40:38 -0500

    added animal component

---
+++ src/app/animal/animal.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { IntakeService, Animal } from "../intake.service";
+
+@Component({
+  selector: "app-animal",
+  templateUrl: "./animal.component.html",
+  styleUrls: ["./animal.component.scss"]
+})
+export class AnimalComponent implements OnInit {
+  id: string;
+  animal: Animal;
+  fullImgUrl: string;
+  loaded = false;
+  SERVER_URL = "https://lacy-ringer.glitch.me";
+  constructor(
+    private intakeService: IntakeService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      const id: string = paramMap.get("id");
+      this.getAnimal(id);
+    });
+  }
+
+  getAnimal(id: string) {
+    this.id = id;
+    this.intakeService.get(id).subscribe(animal => {
+      console.log(animal);
+      this.animal = animal;
+      this.fullImgUrl = `${this.SERVER_URL}/${animal.imgUrl}`;
+      this.loaded = true;
+    });
+  }
+}
