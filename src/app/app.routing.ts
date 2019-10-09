b0c6a13e6bb963eae0e166762a78a9a16f2a204c
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-10-09 09:31:41 -0500

    imported all animals to routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AnimalComponent } from './animal/animal.component';
 import { AddComponent } from './add/add.component';
+import { AllAnimalsComponent } from './all-animals/all-animals.component';
 
 
 const appRoutes: Routes = [
+  { path: 'all', component: AllAnimalsComponent },
   { path: ':id', component: AnimalComponent },
   { path: '', pathMatch: 'full', component: AddComponent }
 ];
