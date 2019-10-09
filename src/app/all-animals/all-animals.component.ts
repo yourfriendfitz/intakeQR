93c08b16c1f323afa9f433fce4287ca4351237b6
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-10-09 09:57:07 -0500

    declared properties and added fetch all animals

--- src/app/all-animals/all-animals.component.ts
+++ src/app/all-animals/all-animals.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { IntakeService } from '../intake.service';
+import { Component, OnInit } from "@angular/core";
+import { IntakeService, Animal } from "../intake.service";
 
 @Component({
-  selector: 'app-all-animals',
-  templateUrl: './all-animals.component.html',
-  styleUrls: ['./all-animals.component.scss']
+  selector: "app-all-animals",
+  templateUrl: "./all-animals.component.html",
+  styleUrls: ["./all-animals.component.scss"]
 })
 export class AllAnimalsComponent implements OnInit {
-
-  constructor(private intakeService: IntakeService) { }
+  animals: Animal[];
+  loaded: boolean;
+  CLIENT_URL = this.intakeService.CLIENT_URL;
+  elementType: "url" | "canvas" | "img" = "url";
+  constructor(private intakeService: IntakeService) {}
 
   ngOnInit() {
+    this.getAnimals();
   }
 
+  getAnimals() {
+    this.intakeService.getAll().subscribe(animals => {
+      console.log(animals);
+      this.animals = animals;
+      this.loaded = true;
+    });
+  }
 }
