57152b917533ea9c711e3b28b7b96263d6b1e2c1
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-10-14 11:12:35 -0500

    sw added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from '@angular/common/http';
-import { NgxQRCodeModule } from 'ngx-qrcode2'; 
-import { routing } from './app.routing';
+import { HttpClientModule } from "@angular/common/http";
+import { NgxQRCodeModule } from "ngx-qrcode2";
+import { routing } from "./app.routing";
 import { AppComponent } from "./app.component";
-import { NavComponent } from './nav/nav.component';
-import { AnimalComponent } from './animal/animal.component';
-import { AddComponent } from './add/add.component';
-import { AllAnimalsComponent } from './all-animals/all-animals.component';
+import { NavComponent } from "./nav/nav.component";
+import { AnimalComponent } from "./animal/animal.component";
+import { AddComponent } from "./add/add.component";
+import { AllAnimalsComponent } from "./all-animals/all-animals.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, AnimalComponent, AddComponent, AllAnimalsComponent],
-  imports: [BrowserModule, ReactiveFormsModule, FormsModule, HttpClientModule, NgxQRCodeModule, routing],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    AnimalComponent,
+    AddComponent,
+    AllAnimalsComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    NgxQRCodeModule,
+    routing,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
